ee5350f246d97f2c3084962ad460a5e5bb2e9729
RamyaShanthi-27 <ramyashanthi@gmail.com>
2024-01-20 22:21:30 +0530

    changes

--- index.js
+++ index.js
@@ -23,12 +23,19 @@ const resetPass = require('./routes/auth/reset');
 
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  exposedHeaders: ["Authorization"],
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send({
