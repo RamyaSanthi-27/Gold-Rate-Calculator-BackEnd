46685dcd7bc1cc3fb9db2e8b6b049dab155e5274
RamyaShanthi-27 <ramyashanthi@gmail.com>
2024-01-20 21:35:56 +0530

    Changes

--- modal/register.js
+++ modal/register.js
@@ -2,14 +2,6 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        default: "",
-    },
     email: {
         type: String,
         required: true,
@@ -27,41 +19,5 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
-    goals: {
-        type: String,
-        required: true,
-    },
-    food:{
-        type : String,
-        required :true,
-    },
-    gender: {
-        type: String,
-        required: true,
-    },
-    age :{
-        type: Number,
-        required: true,
-    },
-    activationLevel :{
-        type: String,
-        required: true,
-    },
-    weight : {
-        type: Array,
-        required: true,
-    },
-    height : {
-        type: Array,
-        required: true,
-    },
-    healthConditionCategory : {
-        type: String,
-        required: true,
-    },
-    healthCondition : {
-        type: String,
-        required: true,
-    }
 })
 module.exports = model("users",userSchema)
